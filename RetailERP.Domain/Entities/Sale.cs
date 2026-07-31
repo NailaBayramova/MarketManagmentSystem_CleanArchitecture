d6f6dd55e58843e3c7576d75fcb622b0c94ae0af
Naila Bayramova <bayramovanaile578@gmail.com>
2026-07-31 04:42:22 -0700

    Add.Methods

--- RetailERP.Domain/Entities/Sale.cs
+++ RetailERP.Domain/Entities/Sale.cs
@@ -1,4 +1,5 @@
 ﻿using RetailERP.Domain.Common;
+using RetailERP.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,11 +8,29 @@ namespace RetailERP.Domain.Entities
 {
     public  class Sale:BaseEntity
     {
-        public Guid  EmployeeId { get;private set; }
-        public Employee Employee { get; private set; }
-        public Guid BranchId { get;private set; }
-        public Branch Branch { get;private set; }
-        public string  InvoiceNumber { get;private set; }
-        public DateTime SaleDate { get; set; }
+        private readonly List<SaleItem> _items = [];
+        public IReadOnlyCollection<SaleItem> Items => _items.AsReadOnly();
+
+        public Guid EmployeeId { get; private set; }
+        public Employee Employee { get; private set; } = null!;
+        public Guid BranchId { get; private set; }
+        public Branch Branch { get; private set; } = null!;
+        public string InvoiceNumber { get; private set; } = null!;
+        public decimal TotalAmount { get; private set; }
+        public PaymentMethod PaymentMethod { get; private set; }
+        public DateTime SaleDate { get; private set; }
+
+        public Sale()
+        {
+            
+        }
+        public Sale(Guid branchId, Guid employeeId, string invoiceNumber, PaymentMethod paymentMethod)
+        {
+            BranchId = branchId;
+            EmployeeId = employeeId;
+            InvoiceNumber = invoiceNumber;
+            PaymentMethod = paymentMethod;
+            SetDate = DateTime.UtcNow;                            
+        }
     }
 }
