4f3f97b140e7c857fb975c746fab7df6599162dd
Naila Bayramova <bayramovanaile578@gmail.com>
2026-09-17 06:41:14 -0700

    ADD.Entities.Configuration

---
+++ RetailERP.Persistence/Configurations/BonusRuleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RetailERP.Domain.Entities;
+
+namespace RetailERP.Persistence.Configurations
+{
+   public sealed class BonusRuleConfiguration : IEntityTypeConfiguration<BonusRule>
+    {
+
+    }
+}
--- RetailERP.Persistence/obj/Debug/net10.0/RetailERP.Persistence.AssemblyInfo.cs
+++ RetailERP.Persistence/obj/Debug/net10.0/RetailERP.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailERP.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9473532a4624993e3c1c7442717b553af98b4806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab13a04735a5d233161de15818c669842ba852eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailERP.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailERP.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
